aee925b3faac59af6e3fd25eacbd373b17f3a0e1
Molfar <sasha.prohor12@gmail.com>
2021-10-03 02:16:09 +0300

    Add project files.

---
+++ 2^200/PowerOfNumber.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace _2_200
+{
+    class PowerOfNumber
+    {
+        private int Number { get; set; }
+        private int Power { get; set; }
+        private LinkedListNode<int> Node;
+        private LinkedList<int> List;
+        private string Value { get; set; }
+
+        private void feelingList()
+        {
+            int sum = 0;
+            int buffer = 0;
+            while (Node != null || buffer > 0)
+            {
+                sum = (Node != null ? Node.Value : 0) * Number + buffer;
+                buffer = 0;
+                if (sum > 9)
+                {
+                    buffer = sum / 10;
+                    sum %= 10;
+                }
+                if (Node == null)
+                    List.AddLast(new LinkedListNode<int>(sum));
+                else
+                    Node.Value = sum;
+                if (Node != null)
+                    Node = Node.Next;
+            }
+            Node = List.First;
+        }
+        public PowerOfNumber(int _number, int _power)
+        {
+            Number = _number;
+            Power = _power;
+            List = new LinkedList<int>();
+            Node = new LinkedListNode<int>(Number);
+            List.AddFirst(Node);
+        }
+        public override string ToString()
+        {
+            if (List.Count > 0)
+            {
+                LinkedListNode<int> lastNodeOfList = List.Last;
+                while (lastNodeOfList.Previous != null)
+                {
+                    Value += lastNodeOfList.Value.ToString();
+                    lastNodeOfList = lastNodeOfList.Previous;
+                }
+                Value += lastNodeOfList.Value.ToString();
+            }
+            else
+                Value = "0";
+            return Value;
+        }
+
+        public string getPower()
+        {
+
+            for (int q = 1; q < Power; ++q)
+            {
+                feelingList();
+            }
+            return this.ToString();
+        }
+    }
+}
---
+++ 2^200/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerOfNumber a = new PowerOfNumber(2, 222);
+
+            Console.WriteLine(a.getPower());
+            //Console.WriteLine("10 {0}",new PowerOfNumber(2, 10));
+            //Console.WriteLine("15 {0}", new PowerOfNumber(2, 15));
+            //Console.WriteLine("20 {0}", new PowerOfNumber(2, 20));
+            //Console.WriteLine("25 {0}", new PowerOfNumber(2, 25));
+
+        }
+    }
+}
