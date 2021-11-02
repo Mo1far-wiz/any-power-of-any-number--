a3e9c73bd4f32327504ea518ce00bd7d66125ef7
Molfar <sasha.prohor12@gmail.com>
2021-11-02 22:17:11 +0200

    misclick

--- 2^200/Program.cs
+++ 2^200/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace _2_200
 {
     class Program
@@ -7,7 +8,7 @@ namespace _2_200
         static void Main(string[] args)
         {
             PowerOfNumber a = new PowerOfNumber(2, 2222);
-            a.
+            
             Console.WriteLine(a.getPower());
             //Console.WriteLine("10 {0}",new PowerOfNumber(2, 10));
             //Console.WriteLine("15 {0}", new PowerOfNumber(2, 15));
